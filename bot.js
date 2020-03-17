452992643e3d8710bd31b9a15ca6fd2f4fc6e816
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-03-17 14:07:12 -0400

    Initial commit

---
+++ bot.js
@@ -0,0 +1,54 @@
+
+
+const Twit = require('twit');
+const config = require('./config.js');
+const tweetBot = new Twit(config);
+
+
+const params = {
+    q: '#classicmovies filter:media',
+    count: 10,
+    lang: 'en'
+}
+
+const favParams = {
+    q: 'criterion channel OR #criterion filter:media',
+    count: 20,
+    lang: 'en'
+
+}
+
+tweetBot.get('search/tweets', params,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Retweet successful`)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+
+tweetBot.get('search/tweets', favParams, (err, data, res) => {
+    if(!err){
+        for(let i=0; i <data.statuses.length; i++) {
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log('Favorite successful')
+                } else {
+                    console.log(err.message)
+                }
+            })
+        }
+    } else {
+        console.log(err)
+    }
+})
